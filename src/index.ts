d236955b8f6ca5830b4f1b1d9b20944bdc0b783f
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2020-09-06 22:48:05 -0300

    Adding dotenv and starting to configure express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Ola Mundo!');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.PORT);
+const app = express();
+const port = process.env.PORT;
+
+app.get('/temp', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
